1ce8eb8aa4c493f6e3163a023c7b330aa85d0f04
labelette <pte@a.com>
2025-07-29 13:49:55 +0200

    dont read bmp180 pressure value if init is false

--- Ook_OSV12/bmp180.h
+++ Ook_OSV12/bmp180.h
@@ -15,12 +15,14 @@ BMP180advanced myBMP(BMP180_ULTRAHIGHRES);
 
 float coefPressureSeaLevel;
 
+bool status;
+
 void bmp180_init()
 {
 #ifdef ESP8266
-        bool status = myBMP.begin(D2,D3); //sda, scl
+        status = myBMP.begin(D2,D3); //sda, scl
 #else
-        bool status = myBMP.begin();
+        status = myBMP.begin();
 #endif
         //compute coeficien niveau de la mer     pressureSeaLevel =  (pressure / pow(1.0 - (float)trueAltitude / 44330, 5.255));
         // coefPressureSeaLevel = 0,991730   1/coefPressureSeaLevel = 1.008338963225878
@@ -32,19 +34,24 @@ void bmp180_init()
             {
                 DbgSerial.println(F("Bosch BMP180/BMP085 is not connected or fail to read calibration coefficients"));
             }
-  
-            DbgSerial.println(F("Bosch BMP180/BMP085 sensor is OK ")); //(F()) saves string to flash & keeps dynamic memory free
-            DbgSerial.print(" coefPressureSeaLevel ");
-            DbgSerial.println(coefPressureSeaLevel,6 );
+            else
+            {
+                DbgSerial.println(F("Bosch BMP180/BMP085 sensor is OK ")); //(F()) saves string to flash & keeps dynamic memory free
+                DbgSerial.print(" coefPressureSeaLevel ");
+                DbgSerial.println(coefPressureSeaLevel,6 );
+            }
         }
 }
 
 void bmp180_read()
 {
-			float temp = myBMP.getTemperature()*10;
-			uint32_t pressureInPa = myBMP.getPressure();
-			reportDomoticTempBaro   ("BMP180", 1,    temp, pressureInPa / 100, pressureInPa / coefPressureSeaLevel/100 , 50.0, 1);
-			reportDomoticTempHumBaro("BMP180", 1, 1, temp, pressureInPa / 100, pressureInPa / coefPressureSeaLevel/100 , 1, 0, 0xff, 0xFF);
+    if (status == true)
+    {
+	    float temp = myBMP.getTemperature()*10;
+	    uint32_t pressureInPa = myBMP.getPressure();
+	    reportDomoticTempBaro   ("BMP180", 1,    temp, pressureInPa / 100, pressureInPa / coefPressureSeaLevel/100 , 50.0, 1);
+    //	reportDomoticTempHumBaro("BMP180", 1, 1, temp, pressureInPa / 100, pressureInPa / coefPressureSeaLevel/100 , 1, 0, 0xff, 0xFF);
+    }
 	
 }
 
